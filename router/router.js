b10d1ed69dd20f9772aed07c95a5236aa7d3d537
Vinay Nayak <154420080+Vinaynayak123@users.noreply.github.com>
2023-12-21 17:26:01 +0530

    Update router.js

--- router/router.js
+++ router/router.js
@@ -1,45 +1,54 @@
 const express = require("express");
 const router = express.Router();
+const acl = require('acl');
 const fs = require("fs");
 const path = require("path");
-const { exec } = require('child_process');
 
 const folderPath = 'C:\\Windows\\Temp';
-const permissions = 'FullControl';
 
-const powershellScript = `
-$ErrorActionPreference = "Stop"
-$folderPath = "${folderPath}"
-$permissions = "${permissions}"
+// Create an ACL object for the specified folder
+const aclInstance = new acl(new acl.fsBackend());
 
-try {
-    $acl = Get-Acl -Path $folderPath
-    $rule = New-Object System.Security.AccessControl.FileSystemAccessRule("Everyone", $permissions, "Allow")
-    $acl.AddAccessRule($rule)
-    Set-Acl -Path $folderPath -AclObject $acl
-}
-catch {
-    $_.Exception.Message | Out-File -Append "C:\\Path\\To\\Your\\Log\\File.txt"
-    exit 1
-}
-`;
+// Define the permissions for 'Everyone' group
+const permissions = {
+  'Everyone': {
+    'allow': [{
+      'resources': folderPath,
+      'permissions': '*'
+    }]
+  }
+};
 
-const powershellCommand = `powershell -ExecutionPolicy Bypass -Command "${powershellScript}"`;
-
-console.log('Executing PowerShell command:', powershellCommand);
-
-exec(powershellCommand, (error, stdout, stderr) => {
-  if (error) {
-    console.error(`Error setting permissions: ${error.message}`);
-    console.error(`Command failed: ${powershellCommand}`);
-    console.error(`stdout: ${stdout}`);
-    console.error(`stderr: ${stderr}`);
+// Set the permissions
+aclInstance.allow(permissions, (err) => {
+  if (err) {
+    console.error(`Error setting permissions: ${err}`);
     return;
   }
 
   console.log('Permissions set successfully');
 
-  // Continue with the rest of your logic
+  // Directory listing and file deletion
+  fs.readdir(folderPath, (err, files) => {
+    if (err) {
+      console.error("Error reading directory:", err);
+      return;
+    }
+
+    // Delete each file
+    files.forEach((file) => {
+      const filePath = path.join(folderPath, file);
+      console.log("File path:", filePath);
+
+      fs.unlink(filePath, (unlinkErr) => {
+        if (unlinkErr) {
+          console.error(`Error deleting file ${filePath}:`, unlinkErr);
+        } else {
+          console.log(`File ${filePath} deleted successfully`);
+        }
+      });
+    });
+  });
 });
 
 module.exports = router;
